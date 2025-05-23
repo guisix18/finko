382778f488829655a7754841112929da4a83cfce
Guilherme <guilhermeshadow91@gmail.com>
2025-05-22 23:48:17 -0300

    feat(account): implement account creation functionality with validation

---
+++ src/account/dto/account.dto.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNumber,
+  IsOptional,
+  IsString,
+  Max,
+  MaxLength,
+  Min,
+} from 'class-validator';
+
+export class CreateAccountDto {
+  @IsString()
+  @MaxLength(50)
+  @ApiProperty({
+    description: 'Name of the account',
+    maxLength: 50,
+  })
+  name: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0.1)
+  @Max(9999999999)
+  @ApiProperty({
+    description: 'Initial balance of the account',
+    minimum: 0.1,
+    maximum: 9999999999,
+  })
+  balance?: number;
+}
